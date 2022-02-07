b2ef14c2f90510f619244463b4d412a0d525a041
Enner Pérez <ennerperez@gmail.com>
2022-02-07 13:05:25 -0500

    Entity Meta

--- samples/BlazorServer/Data/Entities/Employee.cs
+++ samples/BlazorServer/Data/Entities/Employee.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using BlazorServer.Interfaces;
 
 namespace BlazorServer.Data.Entities
@@ -6,10 +8,17 @@ namespace BlazorServer.Data.Entities
     public class Employee : IEntity<int>
     {
         public int Id { get; set; }
+        
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
         public string Position { get; set; }
         public string Office { get; set; }
+        
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayName("Start Date")]
         public DateTime StartDate { get; set; }
         public decimal Salary { get; set; }
     }
